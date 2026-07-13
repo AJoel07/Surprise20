c2b0b83c27c8106deb8f314e3cfed6a05ca34b79
JoelA-07 <2312053@nec.edu.in>
2026-07-13 23:56:13 +0530

    compressed

--- script.js
+++ script.js
@@ -5,48 +5,42 @@ let activeSongPlayer = null;
 const songs = [
   {
     id: "1",
-    title: "Perfect - Ed Sheeran",
-    file:  "assets/Ed Sheeran - Perfect - Ringtone.mp3",
-    note: ""
-  },
-  {
-    id: "2",
     title: "Mudhal Nee Mudivum Nee",
     file:  "assets/mudhal nee mudivum nee.mp3",
     note: ""
   },
   {
-    id: "3",
+    id: "2",
     title: "Aariro Aarariro",
     file:  "assets/aariro aarariro.mp3",
     note: ""
   },
   {
-    id: "4",
+    id: "3",
     title: "Neeyum Naanum",
     file:  "assets/NEEYUM NAANUM.mp3",
     note: ""
   },
   {
-    id: "5",
+    id: "4",
     title: "Pathavaikkum Paarvaikkaara",
     file:  "assets/pathavaikkum paarvaikaara.mp3",
     note: ""
   },
   {
-    id: "6",
+    id: "5",
     title: "Un Viligalil",
     file:  "assets/un viligalil.mp3",
     note: ""
   },
   {
-    id: "7",
+    id: "6",
     title: "Un Viligalil Viluntha Naatkalil",
     file:  "assets/un vilingalil viluntha naatkal.mp3",
     note: ""
   },
   {
-    id: "8",
+    id: "7",
     title: "kaadhal Aasai",
     file:  "assets/Kaadhal aasai.mp4",
     note: ""
